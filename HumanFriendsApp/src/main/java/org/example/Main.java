11e3b64667a7805106c2e1881fbd501ffd0efabe
SmolnyjBuyan <564kot9918@mail.ru>
2024-10-15 07:42:19 +0300

    refactor DataBaseConnect to dynamic variables

--- HumanFriendsApp/src/main/java/org/example/Main.java
+++ HumanFriendsApp/src/main/java/org/example/Main.java
@@ -2,10 +2,9 @@ package org.example;
 
 import org.example.view.MainMenu;
 
-import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
         MainMenu.render();
     }
 }
--- HumanFriendsApp/src/main/java/org/example/db/DataBaseConnect.java
+++ HumanFriendsApp/src/main/java/org/example/db/DataBaseConnect.java
@@ -3,11 +3,16 @@ package org.example.db;
 import java.sql.*;
 
 public class DataBaseConnect {
-    private static final String DB_URL = "jdbc:mysql://localhost:3306/human_friends";
-    private static final String USER = "root";
-    private static final String PASSWORD = "Andrey6542564";
+    private static final String user = System.getenv("MYSQL_USER");
+    private static final String password = System.getenv("MYSQL_PASSWORD");
+    private static final String host = System.getenv("MYSQL_HOST");
+    private static final String port = System.getenv("MYSQL_PORT");
+
+
+    private static final String db_url = "jdbc:mysql://" + host + ":" + port + "/human_friends";
+
 
     public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(DB_URL, USER, PASSWORD);
+        return DriverManager.getConnection(db_url, user, password);
     }
 }
