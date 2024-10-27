271de1cac9b4068188be6e5c20ab70f2fc4efd84
SmolnyjBuyan <154794343+SmolnyjBuyan@users.noreply.github.com>
2024-10-27 11:24:56 +0300

    Delete sql_queries/join_horses_donkeys.sql

--- sql_queries/join_horses_donkeys.sql
+++
@@ -1,7 +0,0 @@
-SELECT * FROM horses
-JOIN pack_animals USING (id)
-JOIN animal_types ON pack_animals.animal_type_id = animal_types.id
-UNION
-SELECT * FROM donkeys
-JOIN pack_animals USING (id)
-JOIN animal_types ON pack_animals.animal_type_id = animal_types.id;
